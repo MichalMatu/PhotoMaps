65337e835e7fc84131cf13b59a30c3ee96463d72
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-13 02:13:54 +0200

    Load content styles in guides route

--- frontend/src/pages/GuidesPage.tsx
+++ frontend/src/pages/GuidesPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import "../styles/content-pages.css";
 import { ExternalLink, MapPinned } from "lucide-react";
 
 import { getGuide, getGuides } from "../api/guides";
