ba6a9159daf0ad79e6ea62e03b1a3272f62db1e2
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-13 08:23:44 +0200

    Allow locking file inputs during async work

--- frontend/src/components/ui/FileInputControl.tsx
+++ frontend/src/components/ui/FileInputControl.tsx
@@ -1,6 +1,7 @@
 type Props = {
   accept: string;
   describedBy?: string;
+  disabled?: boolean;
   file: File | null;
   id?: string;
   inputKey: string;
@@ -8,7 +9,16 @@ type Props = {
   onChange: (file: File | null) => void;
 };
 
-export function FileInputControl({ accept, describedBy, file, id, inputKey, isInvalid = false, onChange }: Props) {
+export function FileInputControl({
+  accept,
+  describedBy,
+  disabled = false,
+  file,
+  id,
+  inputKey,
+  isInvalid = false,
+  onChange,
+}: Props) {
   return (
     <span className={isInvalid ? "file-input-control is-invalid" : "file-input-control"}>
       <span className="file-input-control-button">Wybierz plik</span>
@@ -18,6 +28,7 @@ export function FileInputControl({ accept, describedBy, file, id, inputKey, isIn
         aria-describedby={describedBy}
         aria-invalid={isInvalid}
         className="file-input-control-native"
+        disabled={disabled}
         id={id}
         key={inputKey}
         type="file"
